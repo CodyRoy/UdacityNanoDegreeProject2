406cdddc11488c8864386179420ee37f0bf18640
cr554 <cody.roy@yale.edu>
2017-02-08 11:07:02 -0500

    added button functionality and mad the team scores update to properly display this functionality.

--- app/src/main/java/com/example/cr554/udacitynanodegreeproject2/MainActivity.java
+++ app/src/main/java/com/example/cr554/udacitynanodegreeproject2/MainActivity.java
@@ -2,12 +2,56 @@ package com.example.cr554.udacitynanodegreeproject2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int teamOneScore=0;
+    int teamTwoScore=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display_team_two_score();
+        display_team_one_score();
+
     }
+
+    public void increment_team_one(View view){
+        teamOneScore++;
+        display_team_one_score();
+    }
+    public void increment_team_two(View view){
+        teamTwoScore++;
+        display_team_two_score();
+
+    }
+    public void decrement_team_one(View view){
+        teamOneScore--;
+        display_team_one_score();
+
+    }
+    public void decrement_team_two(View view){
+        teamTwoScore--;
+        display_team_two_score();
+    }
+
+    //both display functions get the appropriate textview, convert the current team score for the appropriate team
+    //from an integer to a string so that it can then set the text displayed to it,
+    public void display_team_one_score(){
+        TextView textView = (TextView) findViewById(R.id.team_one_score);
+        String asString = Integer.toString(teamOneScore);
+        textView.setText(asString);
+        //will update the score for team 1
+        //called from the increment_team_one and decrement_team_one functions
+    }
+    public void display_team_two_score(){
+        TextView textView = (TextView) findViewById(R.id.team_two_score);
+        String asString = Integer.toString(teamTwoScore);
+        textView.setText(asString);
+        //willupdate the score for team 2
+        //called from the increment_team_two and decrement_team_two functions
+    }
+
 }
